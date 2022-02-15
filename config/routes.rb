9db3ed0ed860680ad02ce5c4fa915f6ad77b1592
Rabih Mteirek <rabihmteyrek@hotmail.com>
2022-02-15 15:03:11 -0500

    Added remaining features like edit, delete.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'tasks/new', to: 'tasks#new', as: :new_task
   get 'tasks', to: 'tasks#index', as: :tasks
+
   get 'tasks/:id', to: 'tasks#show', as: :task
+  post 'tasks', to: 'tasks#create'
+
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  patch 'tasks/:id', to: 'tasks#update'
+
+  delete 'tasks/:id', to: 'tasks#destroy', as: :delete_task
 end
